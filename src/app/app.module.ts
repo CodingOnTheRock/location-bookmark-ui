cdc5688a32b76aa1a01908a3b13eaed0ce73d847
Jittichai Chetjaroenchat <coding.ontherock@gmail.com>
2017-07-22 09:38:03 +0700

    Import RouterModule & Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 // Extended Modules
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
